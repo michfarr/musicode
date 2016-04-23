a6c1b457255819c43414e5048faecabbcad1fd3f
michfarr <etnunc@protonmail.com>
2016-04-24 01:46:56 +0200

    Create Controllers

--- app/models/album.rb
+++ app/models/album.rb
@@ -1,4 +1,6 @@
 class Album < ActiveRecord::Base
   belongs_to :artist
   has_many :songs, as: :collection
+
+  validates_presence_of :name, :release_year, :artist_id
 end
--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,4 +1,6 @@
 class Artist < ActiveRecord::Base
   has_many :albums
   has_many :songs, as: :collection
+
+  validates_presence_of :name, :genre, :description
 end
