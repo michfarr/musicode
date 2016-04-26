f394d39d01a3532d20673bf2fe00a80b4979a9c6
michfarr <etnunc@protonmail.com>
2016-04-26 16:36:42 +0200

    Add more styling

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -47,7 +47,7 @@ class ArtistsController < ApplicationController
     redirect_to artists_path
   end
 
-  private
+  protected
 
   def artist_params
     params.require(:artist).permit(:name, :genre, :image, :description)
--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,5 +1,5 @@
 class Artist < ActiveRecord::Base
-  has_many :songs
+  has_many :songs, dependent: :destroy
   accepts_nested_attributes_for :songs
 
   validates :name, :genre, :image, :description, presence: true
